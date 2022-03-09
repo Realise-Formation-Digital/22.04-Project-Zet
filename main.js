efa89faea703c06318c89a7f46db9e9e7af4f422
Gosselke Loic <99717440+Gosselke-Loic@users.noreply.github.com>
2022-03-09 16:56:12 +0100

    Project Fini

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+const liens = document.getElementsByClassName('list-group-item');
+
+Array.from(liens).forEach(element => {
+    element.addEventListener("click", function(){
+        Array.from(liens).forEach(el => {
+            el.classList.remove("active")
+        });
+        element.classList.add("active");
+    });
+});
+
 const displayExtra1 = document.getElementById("extraTotal1");
 displayExtra1.value = 0;
 
