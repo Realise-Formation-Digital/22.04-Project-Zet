5599fe6d11be3006ca40d6f1b5f5deff5f410623
baouad <bachiraouad.aouad@gmail.com>
2022-03-15 14:24:11 +0100

    ok
    ok

--- www/index.php
+++ www/index.php
@@ -31,6 +31,91 @@
                 <span class="navbar-toggler-icon"></span>
               </button>
 
+<<<<<<< Updated upstream
+=======
+              <div class="mdl-bar"> 
+              <!-- Modal Button Log In-->
+              <button type="button" class="btn d-block btn-mdl" data-bs-toggle="modal" data-bs-target="#modalForm">
+                  Log In
+                </button>
+
+                <!-- Modal Log In-->
+                <div class="modal fade" id="modalForm" tabindex="-1" aria-labelledby="exampleModalLabel"
+                  aria-hidden="true">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Sign Up</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">
+                        <form>
+                          <div class="mb-3">
+                            <label class="form-label">Email Address</label>
+                            <input type="text" class="form-control" id="username" name="username"
+                              placeholder="Username" />
+                          </div>
+                          <div class="mb-3">
+                            <label class="form-label">Password</label>
+                            <input type="password" class="form-control" id="password" name="password"
+                              placeholder="Password" />
+                          </div>
+                          <div class="mb-3 form-check">
+                            <input type="checkbox" class="form-check-input" id="rememberMe" />
+                            <label class="form-check-label" for="rememberMe">Remember me</label>
+                          </div>
+                          <div class="modal-footer d-block">
+                            <button type="submit" class="btn btn-warning float-end">Submit</button>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+
+                <!-- Modal Button Sign Up-->
+                <button type="button" class="btn d-block btn-mdl" data-bs-toggle="modal" data-bs-target="#modalForm">
+                  Sign Up
+                </button>
+
+                <!-- Modal Sign Up-->
+                <div class="modal fade" id="modalForm" tabindex="-1" aria-labelledby="exampleModalLabel"
+                  aria-hidden="true">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Log In</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">
+                        <form>
+                          <div class="mb-3">
+                            <label class="form-label">Email Address</label>
+                            <input type="text" class="form-control" id="username" name="username"
+                              placeholder="Username" />
+                          </div>
+                          <div class="mb-3">
+                            <label class="form-label">Password</label>
+                            <input type="password" class="form-control" id="password" name="password"
+                              placeholder="Password" />
+                          </div>
+                          <div class="mb-3 form-check">
+                            <input type="checkbox" class="form-check-input" id="rememberMe" />
+                            <label class="form-check-label" for="rememberMe">Remember me</label>
+                          </div>
+                          <div class="modal-footer d-block">
+                            <button type="submit" class="btn btn-warning float-end">Submit</button>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+
+>>>>>>> Stashed changes
               <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
                 aria-labelledby="offcanvasNavbarLabel">
                 <div class="offcanvas-header">
