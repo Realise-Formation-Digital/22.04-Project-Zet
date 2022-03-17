ebda2e0de58cf445618ea16b03e9d8e7a315c2be
Xavier <dejuan.xavier@gmail.com>
2022-03-17 09:09:50 +0100

    Xavier_Sign_Up_V.1

--- www/index.php
+++ www/index.php
@@ -1,3 +1,9 @@
+<?php
+include "register.php";
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -30,6 +36,89 @@
                 <span style="color:white">MENU</span>
                 <span class="navbar-toggler-icon"></span>
               </button>
+<!-- ----------------------------------------Début ------------------------------------------------------------------------->
+              <div class="mdl-bar"> 
+              <!-- Modal Button Log In-->
+              <button type="button" class="btn d-block btn-mdl" data-bs-toggle="modal" data-bs-target="#modalForm2">
+                  Log In
+                </button>
+
+                <!-- Modal Log In-->
+                <div class="modal fade" id="modalForm2" tabindex="-1" aria-labelledby="exampleModalLabel"
+                  aria-hidden="true">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Log in</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">
+                        <form action="loggin.php" method="post" >
+                          <div class="mb-3">
+                            <label class="form-label">Email Address</label>
+                            <input type="text" class="form-control" id="username" name="username"
+                              placeholder="Username" required />
+                          </div>
+                          <div class="mb-3">
+                            <label class="form-label">Password</label>
+                            <input type="password" class="form-control" id="password" name="password"
+                              placeholder="Password" required />
+                          </div>
+                          <div class="mb-3 form-check">
+                            <input type="checkbox" class="form-check-input" id="rememberMe" />
+                            <label class="form-check-label" for="rememberMe">Remember me</label>
+                          </div>
+                          <div class="modal-footer d-block">
+                            <button type="submit" class="btn btn-warning float-end">Submit</button>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+
+                <!-- Modal Button Sign Up-->
+                <button type="button" class="btn d-block btn-mdl" data-bs-toggle="modal" data-bs-target="#modalForm">
+                  Sign Up
+                </button>
+
+                <!-- Modal Sign Up-->
+                <div class="modal fade" id="modalForm" tabindex="-1" aria-labelledby="exampleModalLabel"
+                  aria-hidden="true">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Sign up</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">
+                      <form action="register.php" method="post" >
+                          <div class="mb-3">
+                            <label class="form-label">Email Address</label>
+                            <input type="text" class="form-control" id="email" name="email"
+                              placeholder="email" required />
+                          </div>
+                          <div class="mb-3">
+                            <label class="form-label">Password</label>
+                            <input type="password" class="form-control" id="pass" name="pass"
+                              placeholder="Password" required />
+                          </div>
+                          <div class="mb-3 form-check">
+                            <input type="checkbox" class="form-check-input" id="rememberMe" />
+                            <label class="form-check-label" for="rememberMe">Remember me</label>
+                          </div>
+                          <div class="modal-footer d-block">
+                            <button type="submit" name="submit" class="btn btn-warning float-end">Submit</button>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <!-- ----------------------------Fin --------------------------------------------- -->
 
               <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
                 aria-labelledby="offcanvasNavbarLabel">
